70480d35fcbeefb4d245310f85d196fe2b30e2c2
shmin <gks831@naver.com>
2022-08-25 02:20:01 +0900

    [RUN-007] Login JWT 구현 2

--- src/main/java/com/august/run/Config/JwtAuthenticationFilter.java
+++ src/main/java/com/august/run/Config/JwtAuthenticationFilter.java
@@ -5,11 +5,13 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.ServletException;
+import lombok.RequiredArgsConstructor;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.filter.GenericFilterBean;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilterBean {
     
     private JwtTokenProvider jwtTokenProvider;
