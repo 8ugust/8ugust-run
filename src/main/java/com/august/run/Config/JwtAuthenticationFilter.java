ff043e03bdec31c9c46e0ae75f76453d454ac121
shmin <gks831@naver.com>
2022-08-31 13:50:50 +0900

    [RUN-007] Login JWT 구현 6

--- src/main/java/com/august/run/Config/JwtAuthenticationFilter.java
+++ src/main/java/com/august/run/Config/JwtAuthenticationFilter.java
@@ -16,6 +16,10 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     
     private JwtTokenProvider jwtTokenProvider;
 
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         String token = jwtTokenProvider.getToken((HttpServletRequest) request);
--- src/main/java/com/august/run/Config/WebSecurityConfig.java
+++ src/main/java/com/august/run/Config/WebSecurityConfig.java
@@ -1,13 +1,14 @@
 package com.august.run.Config;
 
+import org.apache.catalina.security.SecurityConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,6 +16,8 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
     
     @Bean
     public WebSecurityCustomizer configure() {
@@ -25,11 +28,12 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http.httpBasic().disable()
             .csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and().authorizeRequests().anyRequest().permitAll()
-            .and().addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+            .and().addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
 
