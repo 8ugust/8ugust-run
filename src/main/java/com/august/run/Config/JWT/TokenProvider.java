d6b011c72930ffc2e26bae38ca590b43153a8e3f
shmin <gks831@naver.com>
2022-09-23 01:19:32 +0900

    [RUN-013] Spring JWT 2

--- src/main/java/com/august/run/Config/JWT/TokenProvider.java
+++ src/main/java/com/august/run/Config/JWT/TokenProvider.java
@@ -1,5 +1,24 @@
 package com.august.run.Config.JWT;
 
+import java.security.Key;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Component
 public class TokenProvider {
+    private static final String AUTHORITIES_KEY = "auth";
+    private static final String BEARER_TYPE = "bearer";
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;
+    private final Key key;
+
+    public TokenProvider(@Value("${properties.jwt_secret_key}") String secretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
     
 }
