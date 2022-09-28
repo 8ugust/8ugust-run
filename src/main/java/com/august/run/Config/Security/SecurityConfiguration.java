2f907c7a1a38875e845c3a6212561bd94afbb886
shmin <gks831@naver.com>
2022-09-28 14:44:43 +0900

    [RUN-013] Spring JWT 5

--- src/main/java/com/august/run/Config/Security/SecurityConfiguration.java
+++ src/main/java/com/august/run/Config/Security/SecurityConfiguration.java
@@ -1,20 +1,34 @@
 package com.august.run.Config.Security;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.august.run.Config.JWT.TokenProvider;
+import com.august.run.Config.JWT.JwtSecurityConfig;
+import com.august.run.Config.JWT.JwtAccessDeniedHandler;
+import com.august.run.Config.JWT.JwtAuthenticationEntryPoint;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
+@Component
+@Configuration
 @EnableWebSecurity
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class SecurityConfiguration {
     
+    private final TokenProvider tokenProvider;
     private final UserDetailsService userDetailsService;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
     @Bean
     public static BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -24,18 +38,30 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
+            .httpBasic().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            
+            .and()
+            .exceptionHandling()
+            .accessDeniedHandler(jwtAccessDeniedHandler)
+            .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+            
+            .and()
             .authorizeRequests()
-                .antMatchers("/signUp", "/api/user/test").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .permitAll()
-                .loginPage("/login")
-                .and()
-            .logout()
-                .deleteCookies("JSESSIONID")
-                .invalidateHttpSession(true)
-                .clearAuthentication(true);
+            .antMatchers("/auth/**").permitAll()
+            .anyRequest().authenticated()
+
+            .and()
+            .apply(new JwtSecurityConfig(tokenProvider));
+
+            // .formLogin()
+            //     .permitAll()
+            //     .loginPage("/login")
+            //     .and()
+            // .logout()
+            //     .deleteCookies("JSESSIONID")
+            //     .invalidateHttpSession(true)
+            //     .clearAuthentication(true);
 
         return http.build();
     }
---
+++ src/main/java/com/august/run/Config/Security/SecurityUtil.java
@@ -0,0 +1,20 @@
+package com.august.run.Config.Security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityUtil {
+    
+    private SecurityUtil() {
+
+    }
+
+    public static Long getCurrentUserId() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication.getName() == null) {
+            throw new RuntimeException("인증 정보가 없습니다.");
+        }
+
+        return Long.parseLong(authentication.getName());
+    }
+}
